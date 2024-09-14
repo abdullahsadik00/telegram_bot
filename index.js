d06717a10fa00c9d8b151d63849ee6d49f9df7f7
Sadik Shaikh <abdullahsadik00@gmail.com>
2024-09-14 11:27:56 +0530

    Telegram Bot : Initialization

---
+++ index.js
@@ -0,0 +1,5 @@
+const {Telegraf} = require("telegraf")
+const {message} = require("telegraf/filters")
+
+// t.me/coder_js_solution_bot
+const bot = new Telegraf("7119256974:AAGici0WkFOv7Bbs6trSNCZb-RjwjdA2Qgw")
